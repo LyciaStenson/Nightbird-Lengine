7e98b0d44b0b8e5f6b80aeeea38befaec7bba20b
Lycia Stenson <115705933+LyciaStenson@users.noreply.github.com>
2026-03-08 20:27:46 +0000

    Update Main.cpp

--- App/Source/Private/Main.cpp
+++ App/Source/Private/Main.cpp
@@ -31,8 +31,8 @@ int main()
 	std::vector<Core::Vertex> vertices =
 	{
 		{ glm::vec3( 0.0f,  0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.5f, 0.0f), glm::vec2(0), glm::vec2(0) },
-		{ glm::vec3( 0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f), glm::vec2(0), glm::vec2(0) },
-		{ glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f), glm::vec2(0), glm::vec2(0) }
+		{ glm::vec3(-0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(0.0f, 1.0f), glm::vec2(0), glm::vec2(0) },
+		{ glm::vec3( 0.5f, -0.5f, 0.0f), glm::vec3(0.0f, 0.0f, 1.0f), glm::vec2(1.0f, 1.0f), glm::vec2(0), glm::vec2(0) }
 	};
 
 	std::vector<uint16_t> indices = { 0, 1, 2 };
